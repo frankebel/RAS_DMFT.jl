e1d73f3772aeb9046115ca5f71474ed453a6d890
Frank Ebel <frank.ebel@tuwien.ac.at>
2026-07-14 13:30:21 +0200

    test: also test method ambiguities

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,6 +2,6 @@ using Aqua
 using RAS_DMFT
 using Test
 
-@testset verbose = true "Aqua" begin
-    Aqua.test_all(RAS_DMFT; ambiguities = false)
+@testset "Aqua" begin
+    Aqua.test_all(RAS_DMFT)
 end
